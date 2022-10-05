05d6072653a5b68e6060d1d1f01f78ba5c9fe09c
unknown <phornthepsaratis@gmail.com>
2022-10-05 16:07:41 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,127 @@
-import logo from './logo.svg';
+import { v4 as uuidv4 } from 'uuid';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [email,setEmail] = useState('')
+  const [username,setUsername] = useState('')
+  const [password,setPassword] = useState('')
+  const [row,setRow] = useState([])
+  const [checkEditItem,setCheckEditItem]= useState(false)
+  const [editId,setEditId] = useState('')
+  const [checkValid,setCheckValid] = useState(false)
+  const submitData=(e)=>{
+    e.preventDefault();
+    if(checkEditItem){
+      const result = row.map((item)=>{
+        if(item.id === editId){
+          return{
+            ...item,
+            email,
+            username,
+            password
+          }
+        }
+        return item
+      })
+      setRow(result)
+      setEmail('')
+      setUsername('')
+      setPassword('')
+      setEditId('')
+      setCheckEditItem(false)
+      
+    }
+    else{
+      const newItem = {
+      id : uuidv4(),
+      email,
+      username,
+      password
+    }
+    setRow([...row,newItem])
+    setEmail('')
+    setUsername('')
+    setPassword('')
+    setCheckValid(false)
+    }
+  }
+  const removeItem=(id)=>{
+    const result = row.filter(e=>e.id !== id)
+    setRow(result)
+  }
+  const editItem=(id)=>{
+    setCheckEditItem(true)
+    const editRow = row.find(e=>e.id === id)
+    setEditId(editRow.id)
+    setEmail(editRow.email)
+    setUsername(editRow.username)
+    setPassword(editRow.password)
+  }
+  useEffect(()=>{
+    if(!email || !username || !password){
+      setCheckValid(true)
+    }else{
+      setCheckValid(false)
+    }
+  },[email,username,password])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='form-container'>
+        <h1>Registration Form</h1>
+        <form className='form-group' onSubmit={submitData}>
+            <div className='form-control'>
+              <label htmlFor='email'>Email : </label>
+              <input 
+                  type='email' 
+                  id='email' 
+                  onChange={(e)=>setEmail(e.target.value)}
+                  value={email}
+                   />
+            </div>
+            <div className='form-control'>
+              <label htmlFor='username'>Firstname : </label>
+              <input 
+                type='text' 
+                id='username' 
+                onChange={(e)=>setUsername(e.target.value)}
+                value={username}
+                />
+            </div>
+            <div className='form-control'>
+              <label htmlFor='password'>Lastname : </label>
+              <input 
+                type='password' 
+                id='password' 
+                onChange={(e)=>setPassword(e.target.value)}
+                value={password}
+                />
+            </div>
+            <button type='submit' id='btn' disabled={checkValid}>{ checkEditItem ? 'Save Item' : 'Sign up' }</button>
+        </form>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Email</th>
+            <th>Firstname</th>
+            <th>Lastname</th>
+          </tr>
+        </thead>
+        <tbody>
+          {row.map((e,index)=>{
+            return(
+              <tr key={index}>
+                <th>{e.email}</th>
+                <th>{e.username}</th>
+                <th>{e.password}</th>
+                <button class="ix" onClick={()=>editItem(e.id)} >Edit</button>
+                <button class="io" onClick={()=>removeItem(e.id)}>Delete</button>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
